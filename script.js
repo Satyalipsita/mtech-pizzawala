d84d3b7933e051fb39463d4440105b72054d53ce
Satyalipsita <125855990+Satyalipsita@users.noreply.github.com>
2026-01-13 10:03:35 +0530

    Update phone number in order function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function order(item) {
-  const phone = "91XXXXXXXXXX"; // replace with your WhatsApp number
+  const phone = "917978900914; // replace with your WhatsApp number
   const message = `Hello, I want to order ${item} from MTech Pizzawala`;
   const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
   window.open(url, "_blank");
